79ab2612ac2d830e666d210e138ac45adf5b7ed5
EsaulovIl <ilya.esaulov2015@yandex.ru>
2025-06-05 01:50:03 +0300

    Initial commit

--- education/education/urls.py
+++ education/education/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", RedirectView.as_view(url="/education/", permanent=False)),
     path("education/", include("education_program.urls", namespace="education_program")),
 ]
